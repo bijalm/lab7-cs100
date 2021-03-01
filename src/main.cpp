f08588c825c1a0a2cf80f0a2e85acdde425f068a
Keeevini <kevin.ni2000@gmail.com>
2021-02-28 18:13:06 -0800

    Finished tests

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "../headers/parse.hpp"
+
+
+int main(int argv, char** argc) {
+	Factory* factory = new Parse();
+	Base* calc = factory->parse(argc, argv);
+	if (calc) {
+		std::cout << calc->stringify() << " = " << calc->evaluate() << std::endl;
+	}
+	else {
+		std::cout << "Invalid input!" << std::endl;
+	}
+	
+	//std::cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
+	return 0;
+}
